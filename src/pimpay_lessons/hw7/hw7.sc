32b3df5fdf251624af4eebf67ca6381e80868d98
Dan Tereschenko <daniel@tereschenko.info>
2018-06-28 21:16:49 +0300

    hw7

---
+++ src/pimpay_lessons/hw7/hw7.sc
@@ -0,0 +1,12 @@
+import pimpay_lessons.hw7._
+
+val a = MyList(1, 2, 3, 4)
+val b = MyList(9, 8, 7, 6)
+val c = MyList(a, b)
+MyList.head(a)
+MyList.dropWhile(a, (x:Int) => x < 3)
+MyList.drop(a, 3)
+MyList.map(a, (i:Int) => i * 2)
+
+MyList.append(a, b)
+MyList.flatten(c)
---
+++ src/pimpay_lessons/hw7/hw7.scala
@@ -0,0 +1,67 @@
+package pimpay_lessons.hw7
+
+sealed trait MyList[+A]
+
+case object MyNil extends MyList[Nothing]
+
+case class MyCons[A](head: A, tail: MyList[A]) extends MyList[A]
+
+object MyList {
+  def apply[A](as: A*): MyList[A] =
+    if (as.isEmpty) MyNil
+    else MyCons(as.head, apply(as.tail: _*))
+
+  def apply[A]:MyList[A] = MyNil
+
+  def head[A](ls: MyList[A]): A = ls match {
+    case MyCons(h, t) => h
+    case MyNil => throw new Exception
+  }
+
+  def tail[A](ls: MyList[A]): MyList[A] = ls match {
+    case MyCons(h, t) => t
+    case MyNil => throw new Exception
+  }
+
+  def foldRight[A, B](l: MyList[A], z: B)(f: (A, B) => B): B = l match {
+    case MyCons(h, t) => f(h, foldRight(t, z)(f))
+    case MyNil => z
+  }
+
+  def dropWhile[A](l: MyList[A], p: A => Boolean): MyList[A] =
+    foldRight(l, MyList[A])((i, a) => if (p(i)) a else MyCons(i, a))
+
+  def drop[A](l: MyList[A], n: Int): MyList[A] = { //if (n == 0) l else drop(t, n-1)
+    dropWhile(l, (i:A) => n - 1 == 0)
+  }
+
+  def sum(l: MyList[Int]): Int = {
+    foldRight(l, 0)(_ + _)
+  }
+
+  def mul(l: MyList[Int]): Int = {
+    foldRight(l, 1)(_ * _)
+  }
+
+  def size[A](l: MyList[A]): Int = {
+    foldRight(l, 0)((_, n) => n + 1)
+  }
+
+  def map[A,B](ls: MyList[A], f: A => B):MyList[B] =
+    foldRight(ls, MyList[B])((i, a) => MyCons(f(i), a))
+
+  def append[A](a: MyList[A], b: MyList[A]): MyList[A] =
+    foldRight(a, b)((i, b) => MyCons(i, b))//MyCons(h, append(t, b))
+
+  def flatten[A](lss:MyList[MyList[A]]):MyList[A] =
+    foldRight(lss, MyList[A])((i, a) => append(i, a))
+
+  def flatMap[A,B](ls:MyList[A], f: A => MyList[B]):MyList[B] = ???
+  def filter[A](ls:MyList[A], p: A => Boolean):MyList[A] = ???
+  def exists[A](ls:MyList[A], p: A => Boolean):Boolean = ???
+  def reverse[A](ls:MyList[A]):MyList[A] = ???
+
+}
+
+
+
